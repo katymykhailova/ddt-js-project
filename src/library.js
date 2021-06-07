feab8bf325412b5df0441b66a6925a9de9039eee
Puziichuk Olena <nonna467@gmail.com>
2021-06-07 15:14:39 +0300

    add header-library

--- src/library.js
+++ src/library.js
@@ -1,10 +1,6 @@
 import './sass/main.scss';
 
-import './js/components/arrowTop';
-import './js/components/modal';
-import './js/components/modalTeam';
-import './js/components/pagination';
-import './js/fetchMovies';
+
 
 /*кнопка header в library*/
 import './js/components/header-button-library';
