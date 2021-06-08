0d0367cf18e0c822d87616138a907b426040dea3
Puziichuk Olena <nonna467@gmail.com>
2021-06-08 15:32:01 +0300

    added library btn js

--- src/js/components/handle-button-click.js
+++ src/js/components/handle-button-click.js
@@ -1,30 +1,30 @@
-export default class HandleButtonClick {
-  constructor({ selector }) {
-    this.button = this.getButton(selector);
-  }
+// export default class HandleButtonClick {
+//   constructor({ selector }) {
+//     this.button = this.getButton(selector);
+//   }
 
-  getButton(selector) {
-    let button = document.querySelector(selector);
-    return button;
-  }
+//   getButton(selector) {
+//     let button = document.querySelector(selector);
+//     return button;
+//   }
 
-  remove() {
-    this.button.dataset.action = 'add';
-    this.button.classList.remove('add-watched');
-  }
+//   remove() {
+//     this.button.dataset.action = 'add';
+//     this.button.classList.remove('add-watched');
+//   }
 
-  add() {
-    this.button.dataset.action = 'remove';
-    this.button.classList.add('add-watched');
-  }
+//   add() {
+//     this.button.dataset.action = 'remove';
+//     this.button.classList.add('add-watched');
+//   }
 
-  comeIn() {
-    this.button.dataset.action = 'add';
-    this.button.classList.remove('--accent');
-  }
+//   comeIn() {
+//     this.button.dataset.action = 'add';
+//     this.button.classList.remove('--accent');
+//   }
 
-  goOut() {
-    this.button.dataset.action = 'remove';
-    this.button.classList.add('--accent');
-  }
-}
+//   goOut() {
+//     this.button.dataset.action = 'remove';
+//     this.button.classList.add('--accent');
+//   }
+// }
--- src/js/components/header-button-library.js
+++ src/js/components/header-button-library.js
@@ -1,23 +1,44 @@
-import HandleButtonClick from './handle-button-click';
+// import HandleButtonClick from './handle-button-click';
+const refs = {
+    libraryWatchedBtn: document.querySelector('.watched-js'),
+    libraryQueueBtn: document.querySelector('.queue-js'),
+};
 
-const headerBtnWatched = new HandleButtonClick({ selector: '.watched-js' });
+// const headerBtnWatched = new HandleButtonClick({ selector: '.watched-js' });
 
-headerBtnWatched.button.addEventListener('click', () => {
-  if (headerBtnWatched.button.dataset.action === 'add') {
-    return headerBtnWatched.goOut();
-  }
-  if (headerBtnWatched.button.dataset.action === 'remove') {
-    return headerBtnWatched.comeIn();
-  }
-});
+// headerBtnWatched.button.addEventListener('click', () => {
+//   if (headerBtnWatched.button.dataset.action === 'add') {
+//     return headerBtnWatched.goOut();
+//   }
+//   if (headerBtnWatched.button.dataset.action === 'remove') {
+//     return headerBtnWatched.comeIn();
+//   }
+// });
 
-const headerBtnQueue = new HandleButtonClick({ selector: '.queue-js' });
+// const headerBtnQueue = new HandleButtonClick({ selector: '.queue-js' });
 
-headerBtnQueue.button.addEventListener('click', () => {
-  if (headerBtnQueue.button.dataset.action === 'add') {
-    return headerBtnQueue.goOut();
-  }
-  if (headerBtnQueue.button.dataset.action === 'remove') {
-    return headerBtnQueue.comeIn();
-  }
-});
+// headerBtnQueue.button.addEventListener('click', () => {
+//   if (headerBtnQueue.button.dataset.action === 'add') {
+//     return headerBtnQueue.goOut();
+//   }
+//   if (headerBtnQueue.button.dataset.action === 'remove') {
+//     return headerBtnQueue.comeIn();
+//   }
+// });
+
+refs.libraryWatchedBtn.addEventListener('click', libraryWatched);
+refs.libraryQueueBtn.addEventListener('click', libraryQueue);
+  
+function libraryWatched(event) {
+  event.preventDefault();
+ 
+  refs.libraryWatchedBtn.classList.add('is-active');
+  refs.libraryQueueBtn.classList.remove('is-active');
+}
+
+function libraryQueue(event) {
+  event.preventDefault();
+
+  refs.libraryQueueBtn.classList.add('is-active');
+  refs.libraryWatchedBtn.classList.remove('is-active');
+}
