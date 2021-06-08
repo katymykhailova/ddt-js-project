402e682ed0a0f7e6df715962ffa76703ed7002c3
TereshchenkoSD <serghh1988@gmail.com>
2021-06-08 12:13:27 +0300

    Установил плагин модального окна basicLightbox. Изменил значени this.id в конструкторе класса NewsApiService. Написал функции открытия модального окна по клику на элемент галереи и функцию запроса данных с API по id фильма.

---
+++ src/js/components/modal.js
@@ -0,0 +1,33 @@
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/src/styles/main.scss';
+import modalFilmTpl from '../../template/modal-film-card.hbs';
+import MoviesApiService from '../apiService';
+import getRefs from '../refs/get-refs';
+
+const moviesApiService = new MoviesApiService();
+const refs = getRefs();
+
+refs.galleryListEl.addEventListener('click', onModalOpen);
+
+async function fetchMovieDetails(e) {
+  try {
+    const movie = await moviesApiService.fetchMovieDetails();
+    const movieMarkup = modalFilmTpl(movie);
+    const modal = basicLightbox.create(movieMarkup);
+    modal.show();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default function onModalOpen(e) {
+  e.preventDefault();
+
+  const isFilmCardLiEl = e.target.classList.contains('gallery-list__item');
+  if (!isFilmCardLiEl) {
+    return;
+  }
+  console.log(e.target);
+  moviesApiService.id = e.target.dataset.id;
+  fetchMovieDetails();
+}
