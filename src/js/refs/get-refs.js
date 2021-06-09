506290164b7b2fd932f183c79521c31de9aff281
kotikm81 <77439323+kotikm81@users.noreply.github.com>
2021-06-09 20:17:19 +0300

    Добавляет изменение контента при смене страницы

--- src/js/refs/get-refs.js
+++ src/js/refs/get-refs.js
@@ -3,14 +3,12 @@ export default function getRefs() {
     inputEl: document.querySelector('#search-form input'),
     galleryListEl: document.querySelector('.gallery-list'),
     headerEl: document.querySelector('.header-main'),
-    prevPageBtn: document.querySelector('[data-action="prev-page"]'),
-    nextPageBtn: document.querySelector('[data-action="next-page"]'),
     jsWarningEl: document.querySelector('#js-warning'),
-    navigator:document.querySelector('.navigation'),
-    logoBtn:document.querySelector('#logoBtn'),
+    navigator: document.querySelector('.navigation'),
+    logoBtn: document.querySelector('#logoBtn'),
     homeBtn: document.querySelector('#index'),
-    libraryBtn:document.querySelector('#library'),
+    libraryBtn: document.querySelector('#library'),
     form: document.querySelector('#search-form'),
-    buttonBox: document.querySelector("#js-buttons"),
+    buttonBox: document.querySelector('#js-buttons'),
   };
 }
--- src/js/renderPages.js
+++ src/js/renderPages.js
@@ -1,38 +1,44 @@
-
 import getRefs from './refs/get-refs';
+import { fetchPopularMovies } from './fetchApiMovies';
+import { fetchLibraryMovies } from './fetchLibraryMovies';
+
 const refs = getRefs();
 
-refs.navigator.addEventListener('click', loadPageContent)
+refs.navigator.addEventListener('click', loadPageContent);
 function loadPageContent(e) {
-    if (e.target.nodeName !== 'A') {
+  if (e.target.nodeName !== 'A' && e.target.parentNode.nodeName !== 'A') {
     return;
-    }
-    if (e.target.dataset.page === "home") {
+  }
+  if (e.target.dataset.page === 'index') {
     e.preventDefault();
+    history.pushState(null, '', `${e.target.dataset.page}.html`);
     loadHomepageContent();
-    }
-    if (e.target.dataset.page === "library") {
+  }
+  if (e.target.dataset.page === 'library') {
     e.preventDefault();
+    history.pushState(null, '', `${e.target.dataset.page}.html`);
     loadLibraryContent();
-    }
- }
+  }
+}
 
 function loadHomepageContent() {
-    changeClassHeaderComponent();
-    //--Переиспользовать класс или через API.fetch получить результат
-    // и зарендерить в galleryListEl список фильмов для главной страницы///
-};
+  changeClassHeaderComponent();
+  fetchPopularMovies();
+  //--Переиспользовать класс или через API.fetch получить результат
+  // и зарендерить в galleryListEl список фильмов для главной страницы///
+}
 
-function loadLibraryContent()  {
-    changeClassHeaderComponent();
-    //--зарендерить в galleryListEl соответствующий список фильмов watch  или queue для библиотеки //
+function loadLibraryContent() {
+  changeClassHeaderComponent();
+  fetchLibraryMovies();
+  //--зарендерить в galleryListEl соответствующий список фильмов watch  или queue для библиотеки //
 }
 
 function changeClassHeaderComponent() {
-    refs.jsWarningEl.innerHTML = '';
-    refs.headerEl.classList.toggle('my-library');
-    refs.libraryBtn.classList.toggle('current');
-    refs.homeBtn.classList.toggle('current')
-    refs.form.classList.toggle('visually-hidden'); 
-    refs.buttonBox.classList.toggle('visually-hidden');
+  refs.jsWarningEl.innerHTML = '';
+  refs.headerEl.classList.toggle('my-library');
+  refs.libraryBtn.classList.toggle('current');
+  refs.homeBtn.classList.toggle('current');
+  refs.form.classList.toggle('visually-hidden');
+  refs.buttonBox.classList.toggle('visually-hidden');
 }
