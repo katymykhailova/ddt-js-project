f12c19bf9ad0059218cbbdc39028dec87d7f2930
Nataly <natasha.totska@yahoo.com>
2021-06-10 00:22:44 +0300

    Корректирует работу переключения страниц

--- src/js/renderPages.js
+++ src/js/renderPages.js
@@ -22,23 +22,21 @@ function loadPageContent(e) {
 }
 
 function loadHomepageContent() {
-  changeClassHeaderComponent();
+  refs.headerEl.classList.remove('my-library');
+  refs.libraryBtn.classList.remove('current');
+  refs.homeBtn.classList.add('current');
+  refs.form.classList.remove('visually-hidden');
+  refs.buttonBox.classList.add('visually-hidden');
   fetchPopularMovies();
-  //--Переиспользовать класс или через API.fetch получить результат
-  // и зарендерить в galleryListEl список фильмов для главной страницы///
 }
 
 function loadLibraryContent() {
-  changeClassHeaderComponent();
+  refs.headerEl.classList.add('my-library');
+  refs.libraryBtn.classList.add('current');
+  refs.homeBtn.classList.remove('current');
+  refs.form.classList.add('visually-hidden');
+  refs.buttonBox.classList.remove('visually-hidden');
   fetchLibraryMovies();
   //--зарендерить в galleryListEl соответствующий список фильмов watch  или queue для библиотеки //
 }
 
-function changeClassHeaderComponent() {
-  refs.jsWarningEl.innerHTML = '';
-  refs.headerEl.classList.toggle('my-library');
-  refs.libraryBtn.classList.toggle('current');
-  refs.homeBtn.classList.toggle('current');
-  refs.form.classList.toggle('visually-hidden');
-  refs.buttonBox.classList.toggle('visually-hidden');
-}
