41c06c3b30eb93b4bf5bb22520ab7319e6afe9a7
Puziichuk Olena <nonna467@gmail.com>
2021-06-13 11:50:39 +0300

    added sign-in3

--- src/js/components/modal-login.js
+++ src/js/components/modal-login.js
@@ -79,37 +79,6 @@ ui.start('#firebaseui-auth-container', uiConfig);
 
 
 
-//   document.querySelector('.button-logout').onclick = mainApp.logout;
 
 
-// var mainApp = {};
-// (function(){
-// var mainContainer = document.getElementById("main_container");
-
-//     var logtout =  function(){
-//         firebase.auth().signOut().then(function(){
-//             console.log('success');
-//             window.location.replace("header.html");
-//         },function(){})
-//     }
-
-// var init = function(){
-//     firebase.auth().onAuthStateChanged(function(user) {
-//         if (user) {
-//           // User is signed in.
-//           console.log("stay");
-//           mainContainer.style.display = "";
-//         } else {
-//           // No user is signed in.
-//           mainContainer.style.display = "none";
-//           console.log("redirect");
-//           window.location.replace("header.html");
-//         }
-//       });
-// }
-    
-// init();
-
-// mainApp.logout = logtout;
-// })();
 
