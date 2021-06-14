92d3b3aace56b41dfd034652b53581526b3786b8
Puziichuk Olena <nonna467@gmail.com>
2021-06-14 19:20:26 +0300

    added logout

--- src/js/components/modal-login.js
+++ src/js/components/modal-login.js
@@ -43,14 +43,12 @@ function onEscKeyPress(event) {
 }
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC7TRb9mfyMhzQU-yq3pDKTxl2-zaHwRmo',
-  authDomain: 'filmoteka-login.firebaseapp.com',
-  databaseURL:
-    'https://filmoteka-login-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'filmoteka-login',
-  storageBucket: 'filmoteka-login.appspot.com',
-  messagingSenderId: '658952655924',
-  appId: '1:658952655924:web:26304edc0b944079c1e661',
+  apiKey: 'AIzaSyBN4f_F5q6aEuEv1E6c5IHJy5dDCpPJXBo',
+  authDomain: 'filmoteka-f1878.firebaseapp.com',
+  projectId: 'filmoteka-f1878',
+  storageBucket: 'filmoteka-f1878.appspot.com',
+  messagingSenderId: '370619409618',
+  appId: '1:370619409618:web:5a232dc64a1670cf9bf90b',
 };
 firebase.initializeApp(firebaseConfig);
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
@@ -69,7 +67,6 @@ const uiConfig = {
     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
   ],
   callbacks: {
     signInSuccessWithAuthResult: function (authResult) {
