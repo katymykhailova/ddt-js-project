e43b239f78c578aee03339c4d27a5eb0d9c9ead5
Evgen Bielich <belichevgenij@gmail.com>
2021-06-15 19:24:20 +0300

    Slider

---
+++ src/js/components/slider.js
@@ -0,0 +1,43 @@
+new Swiper('.image-slider', {
+   // навигация стрелками
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+   },
+// курсор рука при перетаскивании
+   grabCursor: true,
+// прокрутка клава
+   keyboard: {
+      enabled: true,
+   },
+// прокрутка мышей
+   mousewheel: {
+      sensitivity: 1,
+      eventsTarget: '.image-slider'
+   },
+// кол-во слайдов
+   slidesPerView: 4,
+// отступы между слайдами
+   spaceBetween: 30,
+// кол-во пролистываемых слайдов
+   slidesPerGroup: 1,
+// бесконечный слайд
+   loop: true,
+// свободный режим
+   freeMode: true,
+// автопрокрутка
+   // autoplay: {
+   //    delay: 1500,
+   // },
+// скорость
+   speed: 300,
+// брейкпоинты 
+   breakpoints: {
+      768: {
+         slidesPerView: 4
+      },
+      1024: {
+         slidesPerView: 5
+      }
+   }
+});
