f837aae1af4ec5277f01b3a7222fe291e9f7382d
Sergey <sergey.nosan@gmail.com>
2021-06-15 22:31:00 +0300

    Update spinner.js

--- src/js/components/spinner.js
+++ src/js/components/spinner.js
@@ -2,29 +2,25 @@ import 'spin.js/spin.css';
 import { Spinner } from 'spin.js';
 
 const spinnerOpts = {
-  lines: 14, // The number of lines to draw
-  length: 10, // The length of each line
-  width: 30, // The line thickness
-  radius: 65, // The radius of the inner circle
-  scale: 1, // Scales overall size of the spinner
-  corners: 1, // Corner roundness (0..1)
-  speed: 1.3, // Rounds per second
-  rotate: 0, // The rotation offset
-  animation: 'spinner-line-shrink', // The CSS animation name for the lines
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#ff6b08', // CSS color or array of colors
-  fadeColor: 'transparent', // CSS color or array of colors
-  top: '50%', // Top position relative to parent
-  left: '50%', // Left position relative to parent
-  shadow: '0 0 1px transparent', // Box-shadow for the lines
-  zIndex: 2000000000, // The z-index (defaults to 2e9)
-  className: 'spinner', // The CSS class to assign to the spinner
-  position: 'absolute', // Element positioning
+  lines: 14,
+  length: 10,
+  width: 30,
+  radius: 65,
+  scale: 1,
+  corners: 1,
+  speed: 1.3,
+  rotate: 0,
+  animation: 'spinner-line-shrink',
+  direction: 1,
+  color: '#ff6b08',
+  fadeColor: 'transparent',
+  top: '50%',
+  left: '50%',
+  shadow: '0 0 1px transparent',
+  zIndex: 2000000000,
+  className: 'spinner',
+  position: 'absolute',
 };
-
-export function stopSpinner() {
-  spinner.stop();
-}
 
 let target = document.getElementById('foo');
 let ligtboxSpinner = new Spinner(spinnerOpts).spin(target);
@@ -41,17 +37,20 @@ export function spinnerModal(value = 'start') {
   }
 }
 
-// export function addSpinnerForModalWindow() {
-//   const spinnerForModalWindowEl = document.querySelector('.movie-wrap');
-//   console.log(spinnerForModalWindowEl);
+export function addSpinnerForModalWindow() {
+  const spinnerForModalWindowEl = document.querySelector('.movie-wrap');
 
-//   const spinnerForModalWindow = new Spinner({ ...spinnerOpts, scale: 0.7 }).spin(
-//     spinnerForModalWindowEl,
-//   );
+  const spinnerForModalWindow = new Spinner({ ...spinnerOpts, scale: 0.7 }).spin(
+    spinnerForModalWindowEl,
+  );
 
-//   spinnerForModalWindowEl.childNodes[2].addEventListener('load', stopSpinner);
+  // spinnerForModalWindowEl.childNodes[2].addEventListener('load', stopSpinner);
 
-//   function stopSpinner() {
-//     spinnerForModalWindow.stop();
-//   }
-// }
+  // function stopSpinner() {
+  //   spinnerForModalWindow.stop();
+  // }
+
+  setTimeout(() => {
+    spinnerForModalWindow.stop();
+  }, 1000);
+}
