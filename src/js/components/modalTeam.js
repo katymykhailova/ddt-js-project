ad5e1dae868399d4801e6630862a8d764a5bea1c
kotikm81 <77439323+kotikm81@users.noreply.github.com>
2021-06-16 13:18:11 +0300

    Фиксита анимацию модалки с командой

--- src/js/components/modalTeam.js
+++ src/js/components/modalTeam.js
@@ -58,7 +58,7 @@ const teamModalAnimations = function () {
 }
 
 const teamModalOpen = function() {
-  refs.teamBackdrop.classList.remove('visually-hidden');
+  refs.teamBackdrop.classList.remove('is-hidden');
   refs.teamBackdrop.addEventListener('click',closingBackdropClick)
   teamModalAnimations();
   window.addEventListener('keydown', onEscapePress);
@@ -66,7 +66,7 @@ const teamModalOpen = function() {
 }
 
 const teamModalClose = function() {
-  refs.teamBackdrop.classList.add('visually-hidden');
+  refs.teamBackdrop.classList.add('is-hidden');
   clearInterval(paintCardInterval);
   clearInterval(animateCardInterval);
   window.removeEventListener('keydown', onEscapePress);
